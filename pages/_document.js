aaa0658a78329e680166d61f6e0bb64e2631f1c6
Rohan Baisantry <rohanbaisantry7@gmail.com>
2024-04-11 15:22:21 +0530

    hore: remove code not required

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ const Document = () => {
         {/* favicon */}
         <link rel="shortcut icon" href={favicon} />
         {/* theme meta */}
-        <meta name="theme-name" content="andromeda-light-nextjs" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="theme-color"
